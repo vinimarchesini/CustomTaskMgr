48985f292a3461e01d41f81f1a8baf7fff3417cf
vinimarchesini <95715554+vinimarchesini@users.noreply.github.com>
2021-12-07 16:59:03 -0300

    A simple TaskMgr

---
+++ BootTaskmgr/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace BootTaskmgr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(381, 342);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dê um duplo clique no processo desejado para encerrá-lo.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 392);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerenciador de Processos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BootTaskmgr/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace BootTaskmgr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshlistbox();
+            StartTimer();
+        }
+
+        private void initthread()
+        {
+            System.Threading.Thread rTh = new System.Threading.Thread(new System.Threading.ThreadStart(refreshlistbox));
+            rTh.Start();
+        }
+
+        private void refreshlistbox()
+        {
+
+                if (this.InvokeRequired)
+
+                {
+
+                    this.Invoke(new MethodInvoker(refreshlistbox));
+
+                    return;
+
+                }
+
+                listBox1.Items.Clear();
+                Process[] processes = Process.GetProcesses();
+
+                foreach (Process p in processes)
+                {
+                    if (!String.IsNullOrEmpty(p.MainWindowTitle))
+                    {
+                        listBox1.Items.Add(p.MainWindowTitle);
+                    }
+                }
+            
+        }
+
+        private void StartTimer()
+        {
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            initthread();
+        }
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+
+
+            foreach (Process p in processes)
+            {
+                try {
+                    if (p.MainWindowTitle.Contains(listBox1.SelectedItem.ToString()))
+                    {
+                        DialogResult dialogResult = MessageBox.Show("Tem certeza que deseja encerrar o processo do " + listBox1.SelectedItem.ToString() + "?", "Atenção!", MessageBoxButtons.YesNo);
+                        if (dialogResult == DialogResult.Yes)
+                        {
+                            p.Kill();
+                            return;
+                        }
+                        if (dialogResult == DialogResult.No) {
+                            return;
+                        }
+                    }
+
+                } catch (Exception)
+                {
+                    MessageBox.Show("Não foi possivel localizar o processo. Por favor tente novamente.");
+                    return;
+                }
+            }
+        }
+
+
+    }
+}
